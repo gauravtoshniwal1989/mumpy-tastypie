2a3c6e55478c8da3bcbf79d1a0e59813ef7519a0
Gaurav Toshniwal <gauravtoshniwal1989@gmail.com>
2013-07-18 14:37:59 +0530

    Basic TODO model created

--- todo/models.py
+++ todo/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    todo = models.CharField(max_length=100, blank=False, required=True)
+    todo = models.CharField(max_length=100, blank=False)
     done = models.BooleanField("Done",blank=True, default=False)
+
+    def __unicode__(self):
+    	return "%s" % self.todo
